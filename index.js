478abedd4fd3554d4dcd78a5ec0b99515835b99f
Shubhi <shubhi0906ss@gmail.com>
2022-01-13 22:34:53 +0530

    game

---
+++ index.js
@@ -0,0 +1,60 @@
+const bombs = [];   //global scope
+let gamePoints = 0;
+let canPlay = true;
+
+function updateGamePoints(){
+    const gamePointsElement = document.getElementById('gamePoints');
+    gamePointsElement.innerHTML = "Game Points" +gamePoints;
+}
+
+function addGrid(){
+    const appElement = document.getElementById('app'); //adds rows to app by 'id'
+    for(let i = 0; i < 9; i++){
+        const row = document.createElement('div');
+//       row.style.display = 'flex';
+//       row.style.alignSelf;
+        for(let j = 0; j < 9; j++){
+            const index = i*9+j;
+            const column = document.createElement('div');
+            column.style.display = 'inline-block';
+            column.style.width = '60px';
+            column.style.height = '60px';
+            column.style.border = '1px solid black';
+            column.style.textAlign = 'center';
+            column.style.verticalAlign = 'middle';
+            column.setAttribute("index",index);
+
+            column.addEventListener('click',function(){
+                if(canPlay){ 
+                if(bombs.includes(index)){
+                    alert("Bomb Clicked")
+                    column.innerHTML = 'bomb';
+                    column.style.background = 'red';
+                    canPlay = false;
+                }
+                else{
+                    column.style.background = 'green';
+                    gamePoints++;
+                    updateGamePoints();
+                }
+            }
+            })
+
+            row.appendChild(column);
+        }
+        appElement.appendChild(row);
+    }
+}
+// 11 is taken bcoz the box should be of size 10 
+function generateBombs(){
+    while(bombs.length !== 10){
+        const randomNum = Math.floor(Math.random()*100);
+        if(randomNum < 81 && !bombs.includes(randomNum)){
+            bombs.push(randomNum);
+        }
+    }
+}
+
+addGrid();
+generateBombs();
+console.log(bombs);
